ca3dd1b5bb24a706a8d0fef01f07af62d27a4f52
Vagner Paulino Pinto <46845953+vagnerPaulino@users.noreply.github.com>
2021-06-13 11:15:08 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/personapi/enums/PhoneType.java
@@ -0,0 +1,15 @@
+package one.digitalinnovation.personapi.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+    COMMERCIAL("Commercial"),
+    HOME("Home"),
+    MOBILE("Mobile");
+
+    private final String description;
+}
