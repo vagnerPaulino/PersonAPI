e411369456555c1712d9a51bc5d221ee1b2d31a0
Vagner Paulino Pinto <46845953+vagnerPaulino@users.noreply.github.com>
2021-06-21 18:42:17 -0300

    Developed get person by ID

--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -45,10 +45,6 @@ public class PersonService {
     public PersonDTO findById(Long id) throws PersonNotFoundException {
         Person person = personRepository.findById(id)
                 .orElseThrow(() -> new PersonNotFoundException(id));
-        //Optional<Person> optionalPerson = personRepository.findById(id);
-        //if (optionalPerson.isEmpty()) {
-            //throw new PersonNotFoundException(id);
-        //}
         return personMapper.toDTO(person);
     }
 }
